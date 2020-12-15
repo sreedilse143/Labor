28a317f0f392c50960efa43746cb70af75cab1c7
Acer <Acer@DESKTOP-GUV1GTM.lan>
2020-12-15 11:34:30 +0530

    Data Encryption

--- src/databases/ApacheDataEncrypt.java
+++ src/databases/ApacheDataEncrypt.java
@@ -5,18 +5,17 @@ import java.util.Base64;
 public class ApacheDataEncrypt {
 
 	public static void main(String[] args) {
-		String encrptData= "Password";
-		
+		String encrptData = "Testdata";
+
 		byte[] encodedBytes = Base64.getEncoder().encode(encrptData.getBytes());
-				
-		System.out.println("encodedBytes --------------->" + new String(encodedBytes));
 
-		String decrptData  ="UGFzc3dvcmQ=";
-		
+		String decrptData = new String(encodedBytes).toString();
+
+		System.out.println("encodedBytes --------------->" + decrptData);
+
 		byte[] decodeBytes = Base64.getDecoder().decode(decrptData.getBytes());
-				
-		System.out.println("decodedBytes --------------->" + new String(decodeBytes));
 
+		System.out.println("decodedBytes --------------->" + new String(decodeBytes));
 
 	}
 
